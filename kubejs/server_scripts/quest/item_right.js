2e087e1767e943fcb238d8dfd479119872495a70
Neha YoruNina <836193631@qq.com>
2024-06-07 17:57:23 +0800

    变更属性定义

--- kubejs/server_scripts/quest/item_right.js
+++ kubejs/server_scripts/quest/item_right.js
@@ -2,5 +2,3 @@
 ItemEvents.entityInteracted('minecraft:stick', event => {
     $StartDialog['loadDialog(net.minecraft.world.entity.player.Player,java.lang.String,net.minecraft.world.entity.Entity)'](event.player, 'questapi.example_dialog', event.target)
 })
-
-
--- kubejs/startup_scripts/register/attribute.js
+++ kubejs/startup_scripts/register/attribute.js
@@ -26,19 +26,32 @@ StartupEvents.registry("attribute", event => {
     /**
      * 自然庇佑：影响着所有自然系祈祷能力的效果
      */
-    event.createCustom("kubejs:natrue_bless", () => new $RangedAttribute("Treasure Fortune", 0, 0, 1024).setSyncable(true))
+    event.createCustom("kubejs:natrue_bless", () => new $RangedAttribute("Nature Bless", 0, 0, 1024).setSyncable(true))
     /**
      * 星光庇佑：影响着所有星空系祈祷能力的效果
+     * 包括增强遭遇新生态群系后对祈祷值的提高效果
+     * 在遭遇新的结构时对祈祷值的提高效果
      */
-    event.createCustom("kubejs:starlight_bless", () => new $RangedAttribute("Treasure Fortune", 0, 0, 1024).setSyncable(true))
+    event.createCustom("kubejs:starlight_bless", () => new $RangedAttribute("StarLight Bless", 0, 0, 1024).setSyncable(true))
     /**
      * 洋流庇佑：影响着所有洋流系祈祷能力的效果
+     * 包括增强在获得钓鱼战利品时对祈祷值的提高效果
      */
-    event.createCustom("kubejs:ocean_bless", () => new $RangedAttribute("Treasure Fortune", 0, 0, 1024).setSyncable(true))
+    event.createCustom("kubejs:ocean_bless", () => new $RangedAttribute("Ocean Bless", 0, 0, 1024).setSyncable(true))
     /**
-     * 色彩庇佑：影响着所有色彩系祈祷能力的效果
+     * 鲜血庇佑：影响着所有鲜血系祈祷能力的效果
+     * 包括在击杀怪物后对祈祷值的提高效果
      */
-    event.createCustom("kubejs:colorful_bless", () => new $RangedAttribute("Treasure Fortune", 0, 0, 1024).setSyncable(true))
+    event.createCustom("kubejs:blood_bless", () => new $RangedAttribute("Blood Bless", 0, 0, 1024).setSyncable(true))
+    /**
+     * 命运庇佑：影响着所有命运系祈祷能力的效果
+     * 包括在完成悬赏任务后对祈祷值的提高效果
+     */
+    event.createCustom("kubejs:fate_bless", () => new $RangedAttribute("Fate Bless", 0, 0, 1024).setSyncable(true))
+    /**
+     * 深渊庇佑：影响着所有深渊系祈祷能力的效果
+     */
+    event.createCustom("kubejs:abyss_bless", () => new $RangedAttribute("Abyss Bless", 0, 0, 1024).setSyncable(true))
 })
 
 
@@ -47,6 +60,13 @@ ForgeModEvents.onEvent("net.minecraftforge.event.entity.EntityAttributeModificat
     event.add($EntityType.PLAYER, getAttribute("kubejs:treasure_fortune"))
     event.add($EntityType.PLAYER, getAttribute("kubejs:unpack_ability"))
     event.add($EntityType.PLAYER, getAttribute("kubejs:education_ability"))
+
+    event.add($EntityType.PLAYER, getAttribute("kubejs:natrue_bless"))
+    event.add($EntityType.PLAYER, getAttribute("kubejs:starlight_bless"))
+    event.add($EntityType.PLAYER, getAttribute("kubejs:ocean_bless"))
+    event.add($EntityType.PLAYER, getAttribute("kubejs:blood_bless"))
+    event.add($EntityType.PLAYER, getAttribute("kubejs:fate_bless"))
+    event.add($EntityType.PLAYER, getAttribute("kubejs:abyss_bless"))
 })
 
 
